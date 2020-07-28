8a2ad5af9eb0a0793b701f170e6ddf9025397e57
moshfiqrony <moshfiqrony@gmail.com>
2020-07-29 00:22:39 +0600

    MR | Schema level validation added

--- src/models/User.Model.js
+++ src/models/User.Model.js
@@ -3,17 +3,35 @@ import { Schema, model } from "mongoose";
 const UserSchema = Schema({
     email: {
         type: String,
-        required: true,
-        unique: 'Email already exist'
+        required: 'Email is required',
+        unique: 'Email already exist',
+        trim: true,
+        validate: [
+            {
+                validator: (v) => {
+                    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    return re.test(v);
+                },
+                message: 'Email is not valid'
+            }
+        ]
     },
-    username:{
+    username: {
         type: String,
-        required: true,
-        unique: 'Username already exist'
+        required: 'Username is required',
+        unique: 'Username already exist',
+        trim: true
     },
     password: {
         type: String,
-        required: true,
+        required: 'Password is required',
+        trim: true,
+        validate:[
+            {
+                validator: (v) => (v?.trim()?.length > 6),
+                message: 'Minimum 8 character needed'
+            }
+        ]
     }
 })
 
