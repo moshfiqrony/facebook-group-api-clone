584d934955824676ce50d4557b3b232b5d0b8e77
moshfiqrony <moshfiqrony@gmail.com>
2020-07-30 10:17:30 +0600

    MR | Logout Added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,26 @@
-import {Router} from 'express'
-import UserRoute from './UserRoute'
+import { Router } from 'express'
+import UserRoute from './User.Route'
+import { UNAUTHORIZED, NOT_FOUND, OK } from 'http-status-codes';
+import { prepareError } from '../utils/functions';
+import { TokenModel } from '../models/Token.Model';
 
 const router = Router();
 
 router.use('/user', UserRoute);
+router.route('/logout')
+    .post(async(req, res) => {
+        const user = req?.user;
+        if (!user) {
+            res.status(NOT_FOUND).send(prepareError(
+                'User not found',
+                'Not Found',
+                'user'
+            ));
+        }
+        const logout = await TokenModel.findOneAndUpdate({user: user.id}, {token: ''})
+        if(logout){
+            res.status(OK).send({data: "Success"})
+        }
+    })
 
 export default router;
