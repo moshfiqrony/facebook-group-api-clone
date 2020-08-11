b55eef097a3cab2ca699276a176a394d567867e7
moshfiqrony <moshfiqrony@gmail.com>
2020-08-11 17:33:08 +0600

    MR | Group contoller added

---
+++ src/controllers/Group.Controllers.js
@@ -0,0 +1,54 @@
+import {GroupMetaModel, GroupModel} from '../models/Group.Models'
+import { NOT_FOUND, OK, CREATED, NOT_ACCEPTABLE } from 'http-status-codes'
+import {prepareError} from '../utils/functions';
+
+export const createGroup = async(req, res) => {
+    try {
+        const group = await GroupModel.create({
+            user: req.user.id,
+            name: req.body.name,
+            description: req?.body?.description ?? '',
+            type: req.body.type,
+        })
+        const groupMeta = await GroupMetaModel.create({
+            admins: [user.id],
+            moderator: [],
+            group: group.id
+        })
+        if(group && groupMeta){
+            return res.status(CREATED).send({data: group})
+        }
+    } catch (error) {
+        res.status(NOT_ACCEPTABLE).send({error: error})
+    }
+}
+
+export const getGroup = async(req, res) => {
+    try {
+        const group = await GroupModel.findById(req.params.id);
+        if(group){
+            return res.status(OK).send({data: group})
+        }
+    } catch (error) {
+        res.status(NOT_FOUND).send({error: error})
+    }
+}
+
+
+export const updateGroup = async(req, res) => {
+    if(!req.body.name && !req.body.description && !req.body.type){
+        return res.status(NOT_ACCEPTABLE).send(prepareError('No request body', 'body', 'tracked'));
+    }
+    try {
+        const group = await GroupModel.updateOne(
+            {_id: req.params.id},
+            {...req.body}
+        ).maxTime(10000).exec();
+        if(group){
+            return res.status(OK).send({data: group})
+        }
+    } catch (error) {
+        res.status(NOT_FOUND).send({error: error})
+    }
+}
+
