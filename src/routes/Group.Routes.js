297c04479c78b7fa5ea97150e94b867e4644171c
moshfiqrony <moshfiqrony@gmail.com>
2020-08-15 13:40:15 +0600

    MR | Update Add Remove Route addded

--- src/routes/Group.Routes.js
+++ src/routes/Group.Routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {createGroup, getGroup, updateGroup, getGroups} from '../controllers/Group.Controllers'
+import {createGroup, getGroup, updateGroup, getGroups, addModerator, removeModerator, addMember, removeMember, addAdmin, removeAdmin} from '../controllers/Group.Controllers'
 
 const router = Router();
 
@@ -7,12 +7,17 @@ router.route('/').post(createGroup);
 router.route('/').get(getGroups);
 router.route('/:id').get(getGroup);
 router.route('/:id').put(updateGroup);
+router.route('/:id/moderator').put(addModerator)
+router.route('/:id/moderator').delete(removeModerator)
+router.route('/:id/member').put(addMember)
+router.route('/:id/member').delete(removeMember)
+router.route('/:id/admin').put(addAdmin)
+router.route('/:id/admin').delete(removeAdmin)
 // router.route('/:id/meta').get(getGroupMeta);
 // router.route('/:id/meta').get(createGroupMeta);
 // router.route('/:id').delete(removeGroup);
 // router.route('/:id/admin').get(getAdmins)
 // router.route('/:id/admin').put(addAdmin)
 // router.route('/:id/moderator').get(getModerator)
-// router.route('/:id/moderator').put(addModerator)
 
 export default router;
